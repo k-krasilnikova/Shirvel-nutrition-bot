89e3f8beae6ebcd2603bc6fc55a07af7e903ad4e
Katsiaryna <K.Krasilnikova@itechart-group.com>
2020-12-13 21:55:48 +0300

    Resolved conflicts

--- src/utils.js
+++ src/utils.js
@@ -15,8 +15,9 @@ export const restartApplication = async (bot) => {
 };
 
 export const sendEngryMessage = (bot, user) => {
-  bot.telegram.sendMessage(user, REPLIES.AngryMessage, {
-    parse_mode: "MarkdownV2",
-    disable_notification: true,
-  });
+  console.log("Send angry message to:", user);
+  // bot.telegram.sendMessage(user, REPLIES.AngryMessage, {
+  //   parse_mode: "MarkdownV2",
+  //   disable_notification: true,
+  // });
 };
